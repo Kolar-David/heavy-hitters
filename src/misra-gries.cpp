d0973e87ea8a793137812ee161324d294e1905ff
David Kolář <kolarddk@gmail.com>
2026-03-08 22:42:17 +0100

    Add first (slow)  Misra-gries implementation

---
+++ src/misra-gries.cpp
@@ -0,0 +1,35 @@
+#include "misra-gries.h"
+#include <vector>
+
+MisraGries::MisraGries(countType k, std::string &inputFilePath, std::string &outputFilePath): k(k), Sketch(inputFilePath, outputFilePath) {}
+
+size_t MisraGries::size() {
+    //TODO
+    return 0;
+}
+
+void MisraGries::update(keyType value) {
+    if (counter.find(value) != counter.end()) {
+        ++counter[value];
+    }
+    else {
+        if (counter.size() < k) {
+            counter[value] = 1;
+        }
+        else {
+            for (auto& [key, value] : counter) {
+                --value;
+            }
+            std::erase_if(counter, [](auto const& kv) {
+                return kv.second == 0;
+            });
+
+        }
+    }
+}
+
+void MisraGries::run() {
+    for (auto val : input) {
+        update(val);
+    }
+}
---
+++ src/misra-gries.h
@@ -0,0 +1,18 @@
+#ifndef MISRA_GRIES_H
+#define MISRA_GRIES_H
+#include "sketch.h"
+#include <string>
+#include <unordered_map>
+
+class MisraGries : public Sketch {
+public:
+    MisraGries(countType k, std::string& inputFilePath, std::string& outputFilePath);
+    size_t size() override;
+    void run() override;
+    void saveOutputToFile() override;
+private:
+    countType k;
+    std::unordered_map<keyType, countType> counter;
+    void update(countType val);
+};
+#endif
